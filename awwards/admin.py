01906ae130bf9fdf471486dda51c7672eaace376
victor kigen <vickigen@gmail.com>
2021-05-31 11:33:04 +0300

    added registered models

---
+++ awwards/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.contrib import admin
+from .models import Project, Rating
+
+# Register your models here.
+
+admin.site.register(Project)
+admin.site.register(Rating)

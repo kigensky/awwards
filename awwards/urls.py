fa387145dd36921cbe6658ab0086370a60b70037
victor kigen <vickigen@gmail.com>
2021-05-31 12:05:40 +0300

    added rating delete path

---
+++ awwards/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+from .views import (ProjectListView,ProjectDetailView,ProjectCreateView,ProjectDeleteView,ProjectUpdateView,RatingCreateView,RatingUpdateView,RatingDeleteView)
+
+
+urlpatterns = [
+    path('', ProjectListView.as_view(), name='awwards-home'),
+    path('project/<int:pk>/', ProjectDetailView.as_view(), name='awwards-detail'),
+    path('awwards/new/', ProjectCreateView.as_view(), name='awwards-create'),
+    path('awwards/<int:pk>/update/',ProjectUpdateView.as_view(), name='awwards-update'),
+    path('awwards/<int:pk>/delete/',ProjectDeleteView.as_view(), name='awwards-delete'),
+    path('awwards/<int:pk>/rate/',RatingCreateView.as_view(), name='add_rating'),
+    path('comment/<int:pk>/update/',RatingUpdateView.as_view(), name='rating-update'),
+    path('comment/<int:pk>/delete/',RatingDeleteView.as_view(), name='rating-delete'),
+]
